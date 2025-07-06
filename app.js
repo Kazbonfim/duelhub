33c656e5a32b05c683eeb582cf3c2ff76e34a962
Kazbonfim <zcry.games@gmail.com>
2025-07-06 08:50:22 -0300

    Nova estrutura de arquivos e pastas

--- app.js
+++ app.js
@@ -1,44 +1,14 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 
-// https://fastify.dev/docs/latest/Guides/Getting-Started/
-import Fastify from 'fastify'
-import fastifyStatic from '@fastify/static'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import axios from 'axios'
-import cors from '@fastify/cors'
+import app from './backend/index.js';
 
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const app = Fastify({
-  logger: true,
-});
 const PORT = process.env.PORT || 8080;
 
-await app.register(cors, {
-    origin: true,
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    preflight: true,
-})
-
-app.register(fastifyStatic, {
-    root: path.join(__dirname, 'build'),
-    wildcard: false,
-    index: 'index.html',
-})
-
-// Rotas
-app.get('/teste', function (request, reply) {
-  reply.send('Servidor rodando!')
-})
-
-// Rodando o servidor!
-app.listen({ port: PORT, host: '0.0.0.0' }, function (err, address) {
+app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
-    app.log.error(err)
-    process.exit(1)
+    app.log.error(err);
+    process.exit(1);
   }
-  console.log('Servidor rodando tranquilo, porta: ' + address)
-})
+  console.log(`Servidor rodando tranquilo, porta: ${address}`);
+});
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = Fastify({ logger: true });
+
+await app.register(cors, {
+    origin: true,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflight: true,
+});
+
+// Frontend
+app.register(fastifyStatic, {
+    root: path.join(__dirname, '../frontend/build'),
+    wildcard: false,
+    index: 'index.html',
+});
+
+// Rotas básicas de teste
+app.get('/teste', (req, reply) => {
+    reply.send('Servidor rodando!');
+});
+
+export default app;
